84a3478527ff618f2952ca413c159b2cdb19c5f7
Erin Knox <49617164+erinknox@users.noreply.github.com>
2019-04-18 20:36:36 -0700

    Add files via upload

---
+++ ListHomework/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Lists
+{
+    
+    class Program
+    {
+       
+           
+        static void Main(string[] args)
+        {
+
+
+            var users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "hello" });
+            users.Add(new Models.User { Name = "Steve", Password = "steve" });
+            users.Add(new Models.User { Name = "Lisa", Password = "hello" });
+
+            users.ForEach(u => u.ToString());
+            List<Models.User> passwordList =new List<Models.User>(users.Where(u => u.Password == "hello"));
+           
+            List<Models.User> namePassword = new List<Models.User>(users.Where (u => u.Password == u.Name.ToLower()));
+            users.RemoveAll(u => u.Password == u.Name.ToLower());
+           
+            users.Remove(users.Where(u => u.Password == "hello").FirstOrDefault());
+
+            users.ForEach(u => Console.WriteLine(u.Name +" "+ u.Password));
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
