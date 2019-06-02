f1f3d18f9dce4e6de02166fa2ad816f94aa9b0bf
Erin Knox <49617164+erinknox@users.noreply.github.com>
2019-06-02 16:20:11 -0700

    Add files via upload

---
+++ BirthdayCardGenerator/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CardGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        
+        public ActionResult FormFill()
+        {
+            return View();
+        }
+       [HttpPost]
+        public ActionResult FormFill(Models.CardInfo cardInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("YourCard" , cardInfo);
+            }
+            else
+            {
+                return View();
+            }
+           
+
+        }
+    }
+}
---
+++ BirthdayCardGenerator/Models/CardInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CardGenerator.Models
+{
+    public class CardInfo
+    {
+        [Required(ErrorMessage = "Your name is required")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Recipient's name is required")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Your message is required")]
+        public string Message { get; set; }
+
+    }
+}
